778a524a0cc3b97345fad9495c0c196066560ea9
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-01-22 15:17:12 +0100

    implementation of express, cookie-session and body-parser libraries

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,4 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithbody extends Request {
+  body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  } 
+
+  res.status(403);
+  res.send('Not permitted');
+}
 
 const router = Router();
 
@@ -15,13 +29,51 @@ router.get('/login', (req: Request, res: Response) => {
       </div>
       <button>Submit</button>
     </form>
-  `)
+  `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
-  const {email, password} = req.body;
+router.post('/login', (req: RequestWithbody, res: Response) => {
+  const { email, password } = req.body;
 
-  res.send(email + password);
+  // hardcoded email and pass just to test it
+  if (email && password && email === 'email@me.com' && password === 'pass') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+
 });
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>
+          Logged in!
+          <a href="/logout">Logout</a>
+        </div>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>
+          Not logged in!!
+          <a href="/login">Login</a>
+        </div>
+      </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Protected route, logged-in user');
+})
 
 export { router };
