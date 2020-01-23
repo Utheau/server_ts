e88cc07d9e28c28379aa141aa9786c75017de0ff
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-01-23 14:15:36 +0100

    middlewares and enums implementation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
+import { AppRouter } from './AppRouter';
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import './controllers/LoginController';
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['lax'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('listening port 3000');
